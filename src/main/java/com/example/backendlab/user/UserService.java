54dbf2230b652c3afd90c1915995669ef3ea7651
seoyoung <seoyoung.project@gmail.com>
2026-03-12 22:02:08 +0900

    UserService setting 1/2

--- src/main/java/com/example/backendlab/user/UserService.java
+++ src/main/java/com/example/backendlab/user/UserService.java
@@ -1,4 +1,19 @@
 package com.example.backendlab.user;
 
+import java.time.LocalDateTime;
+
 public class UserService {
+
+    private final UserRepository userRepository = new UserRepository();
+
+    public UserEntity createUser(int id, String username, String email) {
+        UserEntity user = new UserEntity(
+                id,
+                username,
+                email,
+                LocalDateTime.now()
+        );
+
+        return userRepository.save(user);
+    }
 }
